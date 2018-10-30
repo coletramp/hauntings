ed1aacd14cd331ec5fe8196dbfaf5136d7f5894d
Cole Tramp <cole.tramp@my.simpson.edu>
2018-10-30 15:10:09 -0500

    finished

--- public_html/about.php
+++ public_html/about.php
@@ -28,31 +28,8 @@
 
   <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="index.html">SGH</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+     <?php include 'php_include.php'; ?>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-        	<li class="nav-item">
-            	<a class="nav-link" href="index.html">Home</a>
-          	</li>
-
-          	<li class="nav-item active">
-            	<a class="nav-link" href="about.html">About <span class="sr-only">(current)</span></a>
-          	</li>
-
-          	<li class="nav-item">
-            	<a class="nav-link" href="locations.html">Locations</a>
-          	</li>
-        </ul>
-
-        <a class="btn btn-secondary" href="login.html">Login</a>
-          &nbsp;<a class="btn btn-secondary" href="signup.html">Sign-Up</a>
-      </div>
-    </nav>
 
     <!-- Main content -->
     <div class="container">
@@ -76,6 +53,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
+<?php include 'footer.php'; ?>
   </body>
 </html>
--- public_html/contact.php
+++ public_html/contact.php
@@ -26,28 +26,7 @@
 
   <body>
   
-   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="index.html">SGH</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-        	<li class="nav-item">
-            	<a class="nav-link" href="index.html">Home</a>
-          	</li>
-
-          	<li class="nav-item">
-            	<a class="nav-link" href="about.html">About</a>
-          	</li>
-
-          	<li class="nav-item">
-            	<a class="nav-link" href="locations.html">Locations</a>
-          	</li>
-        </ul>
-        </div>
-        </nav>
+    <?php include 'php_include.php'; ?>
         
  <div class="row" id="contact">
 <div class="container mt-5" >
@@ -127,6 +106,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
+<?php include 'footer.php'; ?>
   </body>
 </html>
